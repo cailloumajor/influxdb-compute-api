9b9f3a37db4ccd5dc1de1fe7cf17c9eec019ca46
Arnaud Rocher <arnaud.roche3@gmail.com>
2023-07-03 12:20:04 +0000

    feat: implement healthcheck binary

---
+++ src/bin/healthcheck.rs
@@ -0,0 +1,29 @@
+use anyhow::{anyhow, Context as _};
+use clap::Parser;
+
+use influxdb_compute_api::CommonArgs;
+
+#[derive(Parser)]
+struct Args {
+    #[command(flatten)]
+    common: CommonArgs,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let url = format!(
+        "http://127.0.0.1:{}/health",
+        args.common.listen_address.port()
+    );
+
+    let resp = reqwest::get(url).await.context("request error")?;
+
+    if resp.status().is_success() {
+        Ok(())
+    } else {
+        let body = resp.text().await.context("error getting response body")?;
+        Err(anyhow!(body))
+    }
+}
