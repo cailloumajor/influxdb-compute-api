815da7070f1cf4182b6fa9defa410f178f44acbc
Arnaud Rocher <arnaud.roche3@gmail.com>
2023-10-02 09:49:02 +0200

    test: fix failing tests dur to missing pauses
    The shift that is not the current had no pauses.

--- integration/config-api/config.js
+++ integration/config-api/config.js
@@ -2,16 +2,29 @@ module.exports = () => {
     const locale = "fr-FR";
     const now = new Date();
     const currentShiftStart = new Date(now.getTime() - 8 * 3600000);
-    const nextShiftStart = new Date(currentShiftStart.getTime() + 13 * 3600000);
+    const nextShiftStart = new Date(currentShiftStart.getTime() + 12 * 3600000);
     const shiftStartTimes = [currentShiftStart, nextShiftStart]
         .map((date) => date.toLocaleTimeString(locale))
         .sort();
-    const pausesTimes = [3 * 60, 3 * 60 + 20, 6 * 60, 6 * 60 + 30].map(
-        (startFromShiftMinutes) =>
+    const pausesTimes = [
+        3 * 60,
+        3 * 60 + 20,
+        6 * 60,
+        6 * 60 + 30,
+        15 * 60,
+        15 * 60 + 20,
+        18 * 60,
+        18 * 60 + 30,
+    ]
+        .map((startFromShiftMinutes) =>
             new Date(
                 currentShiftStart.getTime() + startFromShiftMinutes * 60000
             ).toLocaleTimeString(locale)
-    );
+        )
+        .sort();
+
+    console.log("shift start times: %O", shiftStartTimes);
+    console.log("pauses times: %O", pausesTimes);
 
     return {
         common: {
@@ -19,6 +32,8 @@ module.exports = () => {
             pauses: [
                 [pausesTimes[0], pausesTimes[1]],
                 [pausesTimes[2], pausesTimes[3]],
+                [pausesTimes[4], pausesTimes[5]],
+                [pausesTimes[6], pausesTimes[7]],
             ],
             weekStart: {
                 day: "Monday",
